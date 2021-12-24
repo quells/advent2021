704e76928a99dbe057b4398dd3d8aae327e47e62
Kai Wells <support@kaiwells.me>
2021-12-24 11:32:23 -0800

    Day 14B

--- day/14/polymer.go
+++ day/14/polymer.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"bytes"
+	"encoding/binary"
 	"strings"
 )
 
 type rule struct {
-	pair   []byte
+	pair   uint16
 	insert byte
 }
 
@@ -17,7 +17,7 @@ func parse(input string) (state []byte, rules []rule) {
 
 	for _, line := range lines[2:] {
 		parts := strings.Split(line, " -> ")
-		pair := []byte{parts[0][0], parts[0][1]}
+		pair := binary.BigEndian.Uint16([]byte(parts[0]))
 		insert := parts[1][0]
 		rules = append(rules, rule{pair, insert})
 	}
@@ -25,27 +25,51 @@ func parse(input string) (state []byte, rules []rule) {
 	return
 }
 
-func apply(state []byte, rules []rule) []byte {
-	applied := []byte{state[0]}
-	for i := 0; i < len(state)-1; i++ {
-		for _, r := range rules {
-			if bytes.Equal(state[i:i+2], r.pair) {
-				applied = append(applied, r.insert)
-				break
-			}
-		}
-		applied = append(applied, state[i+1])
-	}
-	return applied
+type polymer struct {
+	pairCounts map[uint16]int
+	last       byte
 }
 
-func countFreqs(state []byte) (most, least int) {
-	freqs := make([]int, 128)
-	for _, s := range state {
-		freqs[int(s)]++
+func initPolymer(state []byte) *polymer {
+	pairCounts := make(map[uint16]int)
+	for i := 0; i < len(state)-1; i++ {
+		pair := binary.BigEndian.Uint16(state[i : i+2])
+		pairCounts[pair]++
 	}
+	last := state[len(state)-1]
+	return &polymer{pairCounts, last}
+}
 
-	least = len(state)
+func (p *polymer) apply(rules []rule) {
+	inserted := make(map[uint16]int)
+	for _, r := range rules {
+		count := p.pairCounts[r.pair]
+		if count > 0 {
+			lhs := (r.pair & 0xFF00) | uint16(r.insert)
+			rhs := (r.pair & 0x00FF) | (uint16(r.insert) << 8)
+			inserted[lhs] += count
+			inserted[rhs] += count
+			inserted[r.pair] -= count
+		}
+	}
+	for pair, count := range inserted {
+		p.pairCounts[pair] += count
+		if p.pairCounts[pair] == 0 {
+			delete(p.pairCounts, pair)
+		}
+	}
+}
+
+func (p *polymer) countFreqs() (most, least int) {
+	freqs := make([]int, 128)
+	for pair, count := range p.pairCounts {
+		lhs := (pair & 0xFF00) >> 8
+		freqs[int(lhs)] += count
+		least += count
+	}
+	freqs[int(p.last)]++
+	least++
+
 	for _, f := range freqs {
 		if f > most {
 			most = f
--- day/14/polymer_test.go
+++ day/14/polymer_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	_ "embed"
+	"encoding/binary"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -33,46 +34,71 @@ func Test_parse(t *testing.T) {
 	state, rules := parse(exampleInput)
 	require.Equal(t, "NNCB", string(state))
 	require.Len(t, rules, 16)
-	require.Equal(t, "CH", string(rules[0].pair))
+	require.Equal(t, binary.BigEndian.Uint16([]byte("CH")), rules[0].pair)
 	require.Equal(t, byte('B'), rules[0].insert)
 }
 
-func Test_apply(t *testing.T) {
-	state, rules := parse(exampleInput)
+func Test_initPolymer(t *testing.T) {
+	state, _ := parse(exampleInput)
+	p := initPolymer(state)
 
-	state = apply(state, rules)
-	require.Equal(t, "NCNBCHB", string(state))
-
-	state = apply(state, rules)
-	require.Equal(t, "NBCCNBBBCBHCB", string(state))
-
-	state = apply(state, rules)
-	require.Equal(t, "NBBBCNCCNBBNBNBBCHBHHBCHB", string(state))
-
-	state = apply(state, rules)
-	require.Equal(t, "NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB", string(state))
+	require.Len(t, p.pairCounts, 3)
+	require.Equal(t, byte('B'), p.last)
 }
 
-func Test_countFreqs(t *testing.T) {
+func TestPolymer_apply(t *testing.T) {
+	state, rules := parse(exampleInput)
+	p := initPolymer(state)
+
+	p.apply(rules)
+	require.Equal(t, initPolymer([]byte("NCNBCHB")), p)
+
+	p.apply(rules)
+	require.Equal(t, initPolymer([]byte("NBCCNBBBCBHCB")), p)
+
+	p.apply(rules)
+	require.Equal(t, initPolymer([]byte("NBBBCNCCNBBNBNBBCHBHHBCHB")), p)
+
+	p.apply(rules)
+	require.Equal(t, initPolymer([]byte("NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB")), p)
+}
+
+func TestPolymer_countFreqs(t *testing.T) {
 	t.Run("example", func(t *testing.T) {
 		state, rules := parse(exampleInput)
+		p := initPolymer(state)
 		for i := 0; i < 10; i++ {
-			state = apply(state, rules)
+			p.apply(rules)
 		}
 
-		most, least := countFreqs(state)
+		most, least := p.countFreqs()
 		require.Equal(t, 1749, most)
 		require.Equal(t, 161, least)
+
+		for i := 10; i < 40; i++ {
+			p.apply(rules)
+		}
+		most, least = p.countFreqs()
+		require.Equal(t, 2192039569602, most)
+		require.Equal(t, 3849876073, least)
 	})
 
-	t.Run("puzzle A", func(t *testing.T) {
+	t.Run("puzzle", func(t *testing.T) {
 		state, rules := parse(puzzleInput)
+		p := initPolymer(state)
 		for i := 0; i < 10; i++ {
-			state = apply(state, rules)
+			p.apply(rules)
 		}
 
-		most, least := countFreqs(state)
+		most, least := p.countFreqs()
 		diff := most - least
 		require.Equal(t, 3048, diff)
+
+		for i := 10; i < 40; i++ {
+			p.apply(rules)
+		}
+		most, least = p.countFreqs()
+		diff = most - least
+		require.Equal(t, 3288891573057, diff)
 	})
 }
